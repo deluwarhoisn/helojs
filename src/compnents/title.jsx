ba92343be6c65fea2e0610207651081b9dc23de9
deluwarhoisn <babuhossen301@gmail.com>
2026-02-11 23:17:53 +0600

    children

---
+++ src/compnents/title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const title = ({children}) => {
+    return (
+        <div className='bg-linear-60 from-sky-50 to-sky-200'>
+          <h2 className='text-5xl font-black'> {children}</h2>
+        </div>
+    );
+};
+
+export default title;
