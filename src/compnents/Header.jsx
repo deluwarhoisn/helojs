ffe66672699f331785d3e5952de01b9d7d848a74
deluwarhoisn <babuhossen301@gmail.com>
2026-02-11 23:00:26 +0600

    navbar

---
+++ src/compnents/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+             <header>
+          <h2>app story</h2>
+        </header>
+        </div>
+    );
+};
+
+export default Header;
