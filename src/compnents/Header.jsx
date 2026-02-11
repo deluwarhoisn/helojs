667dbab53540159ab0baefb47f8a9a3ffc93f6a9
deluwarhoisn <babuhossen301@gmail.com>
2026-02-11 23:02:55 +0600

    header

--- src/compnents/Header.jsx
+++ src/compnents/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-             <header>
-          <h2>app story</h2>
-        </header>
+            <header className='px-4 py-3 border-b-2 border-gray-500'>
+                <h2 className='text-lg font-semibold'>app story</h2>
+            </header>
         </div>
     );
 };
